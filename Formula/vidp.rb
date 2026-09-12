2d3cf32b150b14678e8c052dcd6e6b084f8513be
Md Mostafijur Rahman <md.correo.e@gmail.com>
2026-09-13 01:27:12 +0530

    vidp 0.7.0

--- Formula/vidp.rb
+++ Formula/vidp.rb
@@ -1,8 +1,8 @@
 class Vidp < Formula
   desc "Minimal libmpv-based video player for macOS"
   homepage "https://github.com/mdopeace/vidp"
-  url "https://github.com/mdopeace/vidp/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "2dfd5db1a5761df8e84c143593c57e62098c67890397022acd53bcdc8557b391"
+  url "https://github.com/mdopeace/vidp/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "0c8d2fcdf1035dbccf72a58bd431706861d6c4a9ce9171da7d8b8f7429e1130c"
 
   depends_on :macos
   depends_on "mpv"
